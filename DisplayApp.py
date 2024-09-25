7bb2f2e73a08eabf931e2537b40c5a22ca798f98
algotradingtech <prottyttyproton.me@protonmail.com>
2024-09-25 23:47:07 +0200

    Update DisplayApp.py

--- DisplayApp.py
+++ DisplayApp.py
@@ -26,7 +26,7 @@ class R3Widget:
             if st.button("load CSV"):
                 df = pd.read_csv(filePath)
                 df.to_csv("loaded_data.csv", index=False) 
-                st.success("Data correctly insert") if all(["Open","High","Low","Close'] in df.columns ) else st.error("Noms de colonne non comformes => ["Open","High","Low","Close']")
+                st.success("Data correctly insert") if all(["Open","High","Low","Close'] is  in df.columns ) else st.error("Noms de colonne non comformes => ["Open","High","Low","Close']")
                 st.write(df)
 
 
