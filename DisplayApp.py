cb4fe6b8a82563c7d61227114e9c6d78b147c432
algotradingtech <prottyttyproton.me@protonmail.com>
2024-09-26 00:00:00 +0200

    Update DisplayApp.py

--- DisplayApp.py
+++ DisplayApp.py
@@ -25,8 +25,10 @@ class R3Widget:
             filePath =st.file_uploader("Choisissez un fichier CSV", type="csv")
             if st.button("load CSV"):
                 df = pd.read_csv(filePath)
-                df.to_csv("loaded_data.csv", index=False) 
-                if all(["Open","High","Low","Close"]  in df.columns): 
+                check_col_list= [ True if col in df.columns else False for col in ["Open","High","Low","Close"] ]
+                if all(check_col_list): 
+                    
+                    df.to_csv("loaded_data.csv", index=False)
                     st.success("Data correctly insert")  
                 else:
                     st.error("Noms de colonne non comformes => [Open,High,Low,Close]")
